4bb02f685b8df5e335759e8c1216571663672575
Sahil27122 <sahil.exec@gmail.com>
2025-11-16 02:39:42 +0530

    add logics for start, restart, highscore, score, timer

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const board = document.querySelector('.board');
 
+const startButton = document.querySelector('.btn-start');
+const restartButton = document.querySelector('.btn-restart');
+
+const modal = document.querySelector('.modal');
+
+const startGameModal = document.querySelector('.start-game');
+const gameOverModal = document.querySelector('.game-over');
+
+const highScoreElement = document.querySelector("#high-score")
+const scoreElement = document.querySelector("#score")
+const timeElement = document.querySelector("#time")
+
 const blockHeight = 50;
 const blockWidth = 50;
+
+let highScore = localStorage.getItem("highScore") || 0
+let score = 0
+let time = `00-00`
+
+highScoreElement.innerText = highScore
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
 
 let intervalId = null;
+let timerIntervalId = null;
 
 let food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
 
 const blocks = [];
 
-const snake = [
+let snake = [
     {
         x: 1, y: 3
     },
@@ -58,17 +77,34 @@ function render() {
         head = { x: snake[0].x - 1, y: snake[0].y }
     }
 
+    // Wall collision logic
     if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
-        alert("Game Over")
+        // alert("Game Over")
         clearInterval(intervalId)
+
+        modal.style.display = "flex"
+        startGameModal.style.display = "none"
+        gameOverModal.style.display = "flex"
+
+        return;
     }
 
+    // Food consume logic
     if (head.x == food.x && head.y == food.y) {
         blocks[`${food.x}-${food.y}`].classList.remove("food")
         food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
         blocks[`${food.x}-${food.y}`].classList.add("food")
 
         snake.unshift(head)
+
+        score += 10
+        scoreElement.innerText = score
+
+        if(score>highScore){
+            highScore = score
+            localStorage.setItem("highScore", highScore.toString())
+        }
+
     }
 
     snake.forEach(segment => {
@@ -83,9 +119,52 @@ function render() {
     })
 }
 
-intervalId = setInterval(() => {
-    render()
-}, 300)
+// intervalId = setInterval(() => {
+//     render()
+// }, 300)
+
+startButton.addEventListener("click", ()=>{
+    modal.style.display = "none"
+    intervalId = setInterval(()=> {render()}, 200)
+    timerIntervalId = setInterval(()=>{
+        let [min, sec] = time.split("-").map(Number)
+
+        if(sec==59){
+            min+=1
+            sec=0
+        }else{
+            sec+=1
+        }
+
+        time = `${min}-${sec}`
+
+        timeElement.innerText = time
+    }, 1000)
+})
+
+restartButton.addEventListener("click", restartGame)
+
+function restartGame() {
+
+    blocks[`${food.x}-${food.y}`].classList.remove("food")
+    snake.forEach(segment => {
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill')
+    })
+
+    score = 0
+    scoreElement.innerText = score
+
+    time = `00-00`
+    timeElement.innerText = time
+
+    highScoreElement.innerText = highScore
+
+    modal.style.display = "none"
+    direction = "down"
+    snake = [{x:1, y:3}]
+    food = { x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols) }
+    intervalId = setInterval(()=> {render()}, 300)
+}
 
 addEventListener("keydown", (event) => {
     // console.log(event.key)
